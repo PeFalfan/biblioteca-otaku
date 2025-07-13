4c40b4f30966bdc045ddd1e4a43c79232cb7ddb8
Pedro Falfan <pe.falfan@duocuc.cl>
2025-07-13 09:22:30 -0400

    added reader, with controls to go back and forward in a manga

--- src/app/components/manga/manga-reader/manga-reader.component.ts
+++ src/app/components/manga/manga-reader/manga-reader.component.ts
@@ -2,11 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { MangaService } from '../../../services/manga/manga.service';
 import { Router } from '@angular/router';
 import { unzipSync, strFromU8 } from 'fflate';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-manga-reader',
-  imports: [NgFor],
+  imports: [NgFor, NgIf],
   templateUrl: './manga-reader.component.html',
   styleUrl: './manga-reader.component.css'
 })
@@ -15,23 +15,45 @@ export class MangaReaderComponent implements OnInit{
   images: string[] = [];
   mangaName: string = "";
   chapterName: string = "";
+  chaptersList: string[] = [];
+  currentChapterIndex: number = 0;
 
   constructor(private mangaService: MangaService, private router: Router) { }
   
   ngOnInit(): void {
     this.loadMangaInfo();
 
-    this.mangaService.getChapter(this.mangaName, this.chapterName).subscribe((blob) => {
+    this.loadChapters();
+
+    
+
+  }
+
+  loadCurrentChapter() {
+    let manga = this.chaptersList[this.currentChapterIndex].split('/');
+
+
+    this.mangaService.getChapter(manga[0], manga[1]).subscribe((blob) => {
       this.descomprimirCbz(blob);
-    })
+    });
+
+    this.chapterName = manga[1];
+
+    this.router.navigate([`/manga/details/${manga[0]}/${manga[1]}`], {
+      replaceUrl: false,
+      skipLocationChange: false,
+    });
+
   }
 
   loadMangaInfo() {
     const ruta = this.router.url;
     let splits = ruta.split("/");
     console.log(splits);
-    this.mangaName = splits[3];
-    this.chapterName = splits[4];
+    this.mangaName = splits[3].replaceAll('%20', ' ').replaceAll('%28','(').replaceAll('%29', ')');
+    this.chapterName = splits[4].replaceAll('%20', ' ').replaceAll('%28', '(').replaceAll('%29', ')');
+    console.log(this.mangaName);
+    console.log(this.chapterName);
   }
 
   async descomprimirCbz(blob: Blob) {
@@ -42,7 +64,7 @@ export class MangaReaderComponent implements OnInit{
     const extensionesValidas = ['.jpg', '.jpeg', '.png', '.webp'];
     const nombres = Object.keys(zip).filter(name =>
       extensionesValidas.some(ext => name.toLowerCase().endsWith(ext))
-    ).sort(); // orden alfabético por nombre
+    ).sort();
 
     this.images = nombres.map(nombre => {
       const archivo = zip[nombre];
@@ -57,7 +79,31 @@ export class MangaReaderComponent implements OnInit{
   detectarTipoMime(nombre: string): string {
     if (nombre.endsWith('.png')) return 'image/png';
     if (nombre.endsWith('.webp')) return 'image/webp';
-    return 'image/jpeg'; // por defecto
+    return 'image/jpeg';
+  }
+
+  loadChapters() {
+    this.mangaService.getChapters(this.mangaName).subscribe((loadedChapters) => {
+      this.chaptersList = loadedChapters;
+      this.currentChapterIndex = this.chaptersList.indexOf(this.mangaName + '/' + this.chapterName);
+      console.log(this.currentChapterIndex);
+      this.loadCurrentChapter();
+    });
+  }
+
+  previousChapter() {
+    if (this.currentChapterIndex > 0) {
+      this.currentChapterIndex--;
+      this.loadCurrentChapter();
+    }
+    
+  }
+  
+  nextChapter() {
+    if (this.currentChapterIndex < this.chaptersList.length - 1) {
+      this.currentChapterIndex++;
+      this.loadCurrentChapter();
+    }
   }
 
 }
