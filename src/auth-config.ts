779a30918c42e50bc7d2cc30eac82d773476ca22
Pedro Falfan <pe.falfan@duocuc.cl>
2025-07-18 07:06:49 -0400

    Added redirects

--- src/auth-config.ts
+++ src/auth-config.ts
@@ -1,10 +1,10 @@
 // src/auth-config.ts
 export const oidcConfig = {
   authority: 'https://cognito-idp.us-east-1.amazonaws.com/us-east-1_afdY4gkfF',
-  redirectUrl: 'http://localhost:4200',
-  // redirectUrl: 'https://biblioteca-otaku-c6329.web.app',
-  postLogoutRedirectUri: 'http://localhost:4200',
-  // postLogoutRedirectUri: 'https://biblioteca-otaku-c6329.web.app',
+  // redirectUrl: 'http://localhost:4200',
+  redirectUrl: 'https://biblioteca-otaku-c6329.web.app',
+  // postLogoutRedirectUri: 'http://localhost:4200',
+  postLogoutRedirectUri: 'https://biblioteca-otaku-c6329.web.app',
   clientId: '7gk5b0vbt5no9ek8kho0n5gv0f',
   scope: 'openid email',
   responseType: 'code',
