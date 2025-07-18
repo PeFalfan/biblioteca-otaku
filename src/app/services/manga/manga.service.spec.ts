ce632d67d8f2a1a51b784a44b2c1314881fede3e
Pedro Falfan <pe.falfan@duocuc.cl>
2025-07-18 07:33:12 -0400

    added fixes for test

--- src/app/services/manga/manga.service.spec.ts
+++ src/app/services/manga/manga.service.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
 import { MangaService } from './manga.service';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('MangaService', () => {
   let service: MangaService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        MangaService, provideHttpClientTesting(),
+      ]
+    });
     service = TestBed.inject(MangaService);
   });
 
--- src/app/services/video/video.service.spec.ts
+++ src/app/services/video/video.service.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
-
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { VideoService } from './video.service';
 
 describe('VideoService', () => {
   let service: VideoService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [VideoService, provideHttpClientTesting()]
+    });
     service = TestBed.inject(VideoService);
   });
 
